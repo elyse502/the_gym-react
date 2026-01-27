e9e51a7a07823b5c7655727334a166132e2c0ea6
elyse502 <elyseniyibizi502@gmail.com>
2026-01-27 14:28:24 +0200

    refactor(travel-journal): replace single entry prop with spread operator to pass entry fields as individual props to Entry component

--- travel-journal/App.jsx
+++ travel-journal/App.jsx
@@ -4,7 +4,7 @@ import data from "./data";
 
 export default function App() {
   const entryElements = data.map((entry) => {
-    return <Entry key={entry.id} entry={entry} />;
+    return <Entry key={entry.id} {...entry} />;
   });
 
   return (
