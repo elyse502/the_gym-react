f4e0c2cd3be7537f2c4ea50fff66f77ab3c41194
elyse502 <elyseniyibizi502@gmail.com>
2026-03-17 15:25:57 +0200

    chore: configure Tailwind CSS for Redux learning project

--- redux-and-context_api/redux/src/App.jsx
+++ redux-and-context_api/redux/src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Hello, Redux!</h1>
+      <h1 className="font-bold text-xl text-center text-gray-800 underline mt-12">
+        Hello, Redux!
+      </h1>
     </div>
   );
 };
