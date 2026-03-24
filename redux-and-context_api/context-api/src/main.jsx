167ecb209400704e69dcbf362eef88e0d40809d0
elyse502 <elyseniyibizi502@gmail.com>
2026-03-24 10:25:26 +0200

    feat: add Context API example with multiple consumers
        - Create Example2 component demonstrating Context API with two child consumers
    
        - Show Parent1 providing context to Child1 and Child2 components
    
        - Demonstrate Child1 consuming only userName for display
    
        - Show Child2 consuming only setUserName for updates
    
        - Highlight separation of concerns with selective context consumption
    
        - Comment out Example2 in main render for reference
    
        - Add to documentation of Context API usage patterns

--- redux-and-context_api/context-api/src/main.jsx
+++ redux-and-context_api/context-api/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Parent } from "./examples/Example1.jsx";
+import { Parent1 } from "./examples/Example2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     {/* <Parent /> */}
+    {/* <Parent1 /> */}
   </StrictMode>,
 );
