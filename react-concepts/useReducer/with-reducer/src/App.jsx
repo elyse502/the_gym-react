69df0b939b10b846035423308960eb85c31c7e0c
elyse502 <elyseniyibizi502@gmail.com>
2026-04-03 21:31:42 +0200

    refactor: relocate constants and reducer definition after component
        - Move ACTION constants, initialState, and reducer function to end of file
        - Maintain all functionality while reorganizing code structure
        - Keep App component as primary export at top of file
        - Group supporting declarations at bottom for cleaner component focus
        - Improve code readability by separating component logic from helper definitions

--- react-concepts/useReducer/with-reducer/src/App.jsx
+++ react-concepts/useReducer/with-reducer/src/App.jsx
@@ -20,24 +20,6 @@
  */
 import { useReducer } from "react";
 
-const ACTION = {
-  INCREASE: "increase",
-  DECREASE: "decrease",
-};
-
-const initialState = { count: 0 };
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTION.INCREASE:
-      return { count: state.count + 1 };
-    case ACTION.DECREASE:
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
-};
-
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -73,3 +55,21 @@ const App = () => {
 };
 
 export default App;
+
+const ACTION = {
+  INCREASE: "increase",
+  DECREASE: "decrease",
+};
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.INCREASE:
+      return { count: state.count + 1 };
+    case ACTION.DECREASE:
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
