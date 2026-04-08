3d7d4222a2f335b954dcace76cc7ddae3ed5a51b
elyse502 <elyseniyibizi502@gmail.com>
2026-04-08 17:39:24 +0200

    feat: implement product details page with dynamic routing
        - fetch single product using productId from URL
        - display product image, title, price, description, and category
        - handle loading and error states
        - add back navigation for improved UX
        - ensure safe rendering when data is unavailable

--- capstone-projects/ecommerce-app/src/features/products/pages/ProductDetailsPage.tsx
+++ capstone-projects/ecommerce-app/src/features/products/pages/ProductDetailsPage.tsx
@@ -1,9 +1,51 @@
-const ProductDetailsPage = () => {
+import { useParams, useNavigate } from "react-router-dom";
+import { useFetch } from "@/hooks/useFetch";
+import type { Product } from "@/types/product.types";
+
+function ProductDetailsPage() {
+  const { productId } = useParams();
+  const navigate = useNavigate();
+
+  const { data, loading, error } = useFetch<Product>(
+    `https://api.escuelajs.co/api/v1/products/${productId}`,
+  );
+
+  if (loading) return <p className="p-4">Loading product...</p>;
+  if (error) return <p className="p-4 text-red-500">{error}</p>;
+
+  if (!data) return <p className="p-4">Product not found</p>;
+
   return (
-    <div>
-      <h2>Product Details Page</h2>
+    <div className="p-4 space-y-4">
+      {/* Back */}
+      <button onClick={() => navigate(-1)} className="bg-gray-200 px-3 py-1">
+        Back
+      </button>
+
+      {/* Product Info */}
+      <div className="grid md:grid-cols-2 gap-6">
+        {/* Image */}
+        <img
+          src={data.images[0]}
+          alt={data.title}
+          className="w-full h-80 object-cover"
+        />
+
+        {/* Details */}
+        <div className="space-y-3">
+          <h1 className="text-2xl font-bold">{data.title}</h1>
+
+          <p className="text-lg font-semibold">${data.price}</p>
+
+          <p className="text-gray-600">{data.description}</p>
+
+          <p className="text-sm">
+            Category: <span className="font-medium">{data.category.name}</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default ProductDetailsPage;
